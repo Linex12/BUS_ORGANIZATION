2f2f2854c4e327fc59db8b978ddaf0b8e4a20f33
Lyannoy_A_P <lyannoy.a.p1@gmail.com>
2024-10-01 09:19:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BUS_ORGANIZATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            login = new Label();
+            password = new Label();
+            panel1 = new Panel();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 196);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(97, 74);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // login
+            // 
+            login.AutoSize = true;
+            login.Location = new Point(50, 77);
+            login.Name = "login";
+            login.Size = new Size(44, 15);
+            login.TabIndex = 2;
+            login.Text = "Логин:";
+            // 
+            // password
+            // 
+            password.AutoSize = true;
+            password.Location = new Point(42, 199);
+            password.Name = "password";
+            password.Size = new Size(52, 15);
+            password.TabIndex = 3;
+            password.Text = "Пароль:";
+            password.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(login);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(password);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(217, 82);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(299, 280);
+            panel1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(713, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label login;
+        private Label password;
+        private Panel panel1;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace BUS_ORGANIZATION
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+        }
+    }
+}
